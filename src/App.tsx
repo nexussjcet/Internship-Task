ef98f488f517a0b86b74a5e9cc94ff42a88f277d
Vaishakh <vaishakhsubramonain@gmail.com>
2025-05-26 12:08:13 +0000

    added contact section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import FAQ from './components/FAQ';
+import Contacts from './components/Contacts';
 import Footer from './components/Footer';
 
 
@@ -16,6 +17,7 @@ function App() {
       <Hero/>
       <About/>
       <FAQ/>
+      <Contacts/>
       <Footer/>
     </div>
   )
---
+++ src/components/Contacts.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './styles/Contacts.css'
+
+const Contacts: React.FC = () =>{
+    return(
+        <section className="Contacts">
+            <h1>
+            ContactUS    
+            </h1>
+            <div className="mail-id">
+                <p>E-mail us at</p>
+                <p><strong>hacksphere2025@gmail.com</strong></p>
+            </div>
+            <div className="phone-no">
+                <p>Connect to us via Phone/Whatsapp at</p>
+                <p><strong>+91 1234567890</strong></p>
+            </div>
+        </section>
+
+    );
+};
+
+export default Contacts;
