f8452f50a9a876b2ff88175bf0c752d9ee3af111
amy <amyanup6@gmail.com>
2025-06-04 16:39:56 +0530

    Add complete ToDo app

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/", // Adjust this if deploying to subfolder
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,4 +11,3 @@ export default defineConfig({
     },
   },
 });
-
