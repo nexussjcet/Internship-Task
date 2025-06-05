20035fe006f3bab712d57a514279a3ddcb9b4c1a
Alphons Biju <alphonsbiju.tech@gmail.com>
2025-06-05 19:34:45 +0530

    Add files via upload

---
+++ AlphonsBiju-TodoApp/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <br></br>
+      <Todos />
+    
+    </div>
+  );
+}
+
+export default App;
